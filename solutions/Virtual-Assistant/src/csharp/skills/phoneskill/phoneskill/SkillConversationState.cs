48fac9374c627ec191097585a332df2c7f0adaeb
Marion Zepf <mazepf@microsoft.com>
2019-02-22 12:04:00 -0500

    Phone: New skill to make phone calls

---
+++ solutions/Virtual-Assistant/src/csharp/skills/phoneskill/phoneskill/SkillConversationState.cs
@@ -0,0 +1,20 @@
+﻿using Luis;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace PhoneSkill
+{
+    public class SkillConversationState : DialogState
+    {
+        public SkillConversationState()
+        {
+        }
+
+        public string Token { get; internal set; }
+
+        public PhoneSkillLU LuisResult { get; set; }
+
+        public void Clear()
+        {
+        }
+    }
+}
